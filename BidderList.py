355a450e6abd78ed6295e70572e87d750fe638c8
Jou Barzdukas <joubarzdukas@gmail.com>
2025-07-13 00:40:14 -0400

    Add adhoc reserve price option

--- BidderList.py
+++ BidderList.py
@@ -1,10 +1,12 @@
 import Distribution
 import Bidder
 class BidderList:
-    def __init__(self):
+    def __init__(self, reserve_price=None):
         self.bidders = []
         self.current_id_count = 0
         self.distribution = Distribution.Distribution("equal_revenue")
+        if reserve_price is not None:
+            self.add_reserve_price(reserve_price)
 
     def add_n_bidders(self, n):
         for _ in range(n):
@@ -19,3 +21,7 @@ class BidderList:
         new_bidder = Bidder.Bidder(self.current_id_count, self.distribution.sample())
         self.bidders.append(new_bidder)
         self.current_id_count += 1
+    
+    def add_reserve_price(self, reserve_price):
+        new_bidder = Bidder.Bidder(-1, reserve_price)
+        self.bidders.append(new_bidder)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import BidderList
 
 def main():
-    bidder_list = BidderList.BidderList()
+    # Reserve price 1
+    bidder_list = BidderList.BidderList(1)
+    # 5 bidders
     bidder_list.add_n_bidders(5)
 
     for bidder in bidder_list.bidders:
