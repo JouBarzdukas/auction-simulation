ebfbf7c18cbce0fb3e5394f6c4a7a0cf12e43993
Jou Barzdukas <joubarzdukas@gmail.com>
2025-07-13 00:28:01 -0400

    Fix bug with calling classes

--- BidderList.py
+++ BidderList.py
@@ -4,18 +4,18 @@ class BidderList:
     def __init__(self):
         self.bidders = []
         self.current_id_count = 0
-        self.distribution = Distribution("equal_revenue")
+        self.distribution = Distribution.Distribution("equal_revenue")
 
     def add_n_bidders(self, n):
         for _ in range(n):
             self.add_extra_real_bidder()
 
     def add_extra_real_bidder(self):
-        new_bidder = Bidder(self.current_id_count, self.istribution.sample())
+        new_bidder = Bidder.Bidder(self.current_id_count, self.distribution.sample())
         self.bidders.append(new_bidder)
         self.current_id_count += 1
 
     def add_extra_fake_bidder(self):
-        new_bidder = Bidder(self.current_id_count, self.distribution.sample())
+        new_bidder = Bidder.Bidder(self.current_id_count, self.distribution.sample())
         self.bidders.append(new_bidder)
         self.current_id_count += 1
