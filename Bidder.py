7f4bafcfa4b85f1947d424bf2e4365616493ebc4
Jou Barzdukas <joubarzdukas@gmail.com>
2025-07-21 21:52:46 -0400

    Update bidder class to take into account revealing logic

--- Bidder.py
+++ Bidder.py
@@ -2,5 +2,11 @@ class Bidder:
     def __init__(self, id, value, bidder_type="real"):
         self.id = id
         self.value = value
-        self.bidder_type = bidder_type 
-        # Real OR "Auctioneer" for bidder_type
+        self.bidder_type = bidder_type
+        self.revealed = False
+        self.paid_fine = 0.0
+
+    def decide_reveal(self, current_highest, fine):
+        if self.bidder_type in {"real", "reserve"}:
+            return True
+        return self.value < current_highest
