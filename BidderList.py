0097025439936363200f86431d264fa134a90a42
Jou Barzdukas <joubarzdukas@gmail.com>
2025-07-21 21:51:59 -0400

    Add reserve price and ordering for bidderlist

--- BidderList.py
+++ BidderList.py
@@ -3,7 +3,7 @@ import Bidder
 
 class BidderList:
     def __init__(self, reserve_price=None):
-        self.bidders = {}  # Mapping bidder id to bidder instance
+        self.bidders = {} 
         self.current_id_count = 0
         self.distribution = Distribution.Distribution("equal_revenue")
         if reserve_price is not None:
@@ -21,14 +21,14 @@ class BidderList:
         self.bidders[self.current_id_count] = new_bidder
         self.current_id_count += 1
 
-    def add_extra_fake_bidder(self):
-        new_bidder = Bidder.Bidder(self.current_id_count, self.distribution.sample(), "auctioneer")
+    def add_extra_fake_bidder(self, value=None):
+        v = value if value is not None else self.distribution.sample()
+        new_bidder = Bidder.Bidder(self.current_id_count, v, "auctioneer")
         self.bidders[self.current_id_count] = new_bidder
         self.current_id_count += 1
 
     def add_reserve_price(self, reserve_price):
-        new_bidder = Bidder.Bidder(-1, reserve_price, "auctioneer")
-        self.bidders[-1] = new_bidder
+        self.bidders[-1] = Bidder.Bidder(-1, reserve_price, "reserve")
 
     def get_bidder_by_id(self, bidder_id):
         return self.bidders.get(bidder_id)
